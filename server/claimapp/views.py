34e6e71ceaa0f8bcf6defe44fb68bdb3c4e16bea
sunguangning <sun-guangning@126.com>
2020-03-26 00:33:19 +0800

    修改申领记录创建方式增加for循环

--- server/claimapp/views.py
+++ server/claimapp/views.py
@@ -57,36 +57,37 @@ def asset_detail(request):
 def claim_asset(request):
     if request.method == 'POST':
         # claim_username = request.POST['claim_username']
-        claim_count = request.POST['claim_count']
-        # claim_phone_num = request.POST['claim_phone_num']
-        claim_name = request.POST['claim_name']
-        category = request.POST['category']
-        try:
-            assetinfo = AssetInfo.objects.get(asset_name=claim_name)
-            # import pdb;pdb.set_trace()
-            # 查看申领物品剩余是否足量
-            if int(assetinfo.asset_count)<int(claim_count):
-                return _generate_json_message(False,""+claim_name+"库存商品不足")
-            # 查看该部门是否有权限申领该数量
-            if int(claim_count)> 5:
-                return _generate_json_message(False,"抱歉,该部门没有权限申请过多商品")
-            assetinfo.asset_count = int(assetinfo.asset_count) - int(claim_count)
-            # 资产管理减少指定数量物品
-            assetinfo.save()
-            # 创建申领记录
-            claimrecord = ClaimRecord(
-                                    # claim_username=claim_username,
-                                    # claim_weixin_id=weixin_id,
-                                    claim_count=claim_count,
-                                    # claim_phone_num=claim_phone_num,
-                                    claim_name=claim_name,
-                                    category=Category.objects.get(id=category),
-                                    )
-            claimrecord.save()
-            return _generate_json_message(True, "申领成功")
-            
-        except :
-            return _generate_json_message(False, "仓库中没有该类型商品")
+        claim_list = request.POST['choose_list']
+        for claim_submmit in claim_list:
+            claim_count = claim_submmit['claim_count']
+            # claim_phone_num = request.POST['claim_phone_num']
+            claim_name = claim_submmit['claim_name']
+            category = claim_submmit['category']
+            try:
+                assetinfo = AssetInfo.objects.get(asset_name=claim_name)
+                # import pdb;pdb.set_trace()
+                # 查看申领物品剩余是否足量
+                if int(assetinfo.asset_count)<int(claim_count):
+                    return _generate_json_message(False,""+claim_name+"库存商品不足")
+                # 查看该部门是否有权限申领该数量
+                if int(claim_count)> 5:
+                    return _generate_json_message(False,"抱歉,该部门没有权限申请过多商品")
+                assetinfo.asset_count = int(assetinfo.asset_count) - int(claim_count)
+                # 资产管理减少指定数量物品
+                assetinfo.save()
+                # 创建申领记录
+                claimrecord = ClaimRecord(
+                                        # claim_username=claim_username,
+                                        # claim_weixin_id=weixin_id,
+                                        claim_count=claim_count,
+                                        # claim_phone_num=claim_phone_num,
+                                        claim_name=claim_name,
+                                        category=Category.objects.get(id=category),
+                                        )
+                claimrecord.save()
+                return _generate_json_message(True, "申领成功")
+            except :
+                return _generate_json_message(False, "仓库中没有该类型商品")
     
 
 # 用户注册功能
